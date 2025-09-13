0fdb91dfd841c5f8a503ae3839f0f478411670de
Adilbek <adilbek.kass@gmail.com>
2025-09-13 12:05:58 +0500

    Completed Admin Dashboard v1

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,62 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { prisma } from "../../../lib/prisma";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { get } from "http";
 
-export default function adminDashboard() {
+async function getSalesData(){
+    const data = await prisma.order.aggregate({
+        _sum: {
+            pricePaidInCents: true
+        },
+        _count: true
+    })
+    await wait(2000); // simulate slow db
+    return {
+        amount: (data._sum.pricePaidInCents || 0) / 100,
+        numberOfSales: data._count 
+    }
+}
+
+function wait(ms: number){
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getUserData(){
+    const [userCount, orderData] = await Promise.all([
+        prisma.user.count(),
+        prisma.order.aggregate({
+            _sum: {pricePaidInCents: true}})
+    ])
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : ((orderData._sum.pricePaidInCents || 0) / 100) / userCount
+    }
+}
+
+async function getProductData(){
+    const [activeCount, inactiveCount] = await Promise.all([
+        prisma.product.count({where: {isAvailableForPurchase: true}}),
+        prisma.product.count({where: {isAvailableForPurchase: false}})
+    ])
+    return {
+        activeCount,
+        inactiveCount
+    }
+}
+
+
+export default async function adminDashboard() {
+    const [salesData, userData, productData] = await Promise.all([
+        getSalesData(),
+        getUserData(),
+        getProductData()
+    ])
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <DashboardCard title="Sales" subtitle="test" body="body"></DashboardCard>
-            <DashboardCard title="Sales" subtitle="test" body="body"></DashboardCard>
-            <DashboardCard title="Sales" subtitle="test" body="body"></DashboardCard>
+            <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} body={formatCurrency(salesData.amount)}></DashboardCard>
+            <DashboardCard title="Customers" subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`} body={formatNumber(userData.userCount)}></DashboardCard>
+            <DashboardCard title="Active Products" subtitle={`${formatNumber(productData.inactiveCount)} Inactive`} body={formatNumber(productData.activeCount)}></DashboardCard>
+
     </div>
     )
 }
